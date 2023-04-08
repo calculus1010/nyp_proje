cb3fb9bae9b3c99d703b850d75ad13f8fd42deb7
calculus1010 <117119873+calculus1010@users.noreply.github.com>
2023-04-09 01:46:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,51 @@
 import java.io.*;
 import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    private String elitKisiBilgileri;
+    private String genelKisiBilgileri;
+    public String getElitKisiBilgileri(){
+        return this.elitKisiBilgileri;
+    }
+    public void setElitKisiBilgileri(String elitKisiBilgileri){
+        this.elitKisiBilgileri=elitKisiBilgileri;
+    }
+    public String getgenelKisiBilgileri(){
+        return this.genelKisiBilgileri;
+    }
+    public void setgenelKisiBilgileri(String genelKisiBilgileri){
+        this.genelKisiBilgileri=genelKisiBilgileri;
+    }
+    public static void main(String[] args) throws IOException
+    {
+        //diğer clasların nesneleri
+        Main main=new Main();
         DosyaIslemleri dosyaIslemleri=new DosyaIslemleri();
         MailGonderme mailGonderme=new MailGonderme();
         System.out.println("lütfen yapmak istediğiniz işlemi seçiniz \n1-elit üye ekleme\n2-Genel üye ekleme\n3-Mail gönderme");
         //1.menü
         Scanner input=new Scanner(System.in);
         int a= input.nextInt();
-        switch (a){
+        switch (a)
+            {
             case 1:
-                dosyaIslemleri.elitUyeEkleme();
+                System.out.println("lütfen ad soyad email giriniz");
+                Scanner b =new Scanner(System.in);
+                String elitKisiBilgileri=b.nextLine();
+                dosyaIslemleri.elitUyeEkleme(elitKisiBilgileri);
                 break;
             case 2:
-                dosyaIslemleri.genelUyeEkleme();
+                System.out.println("lütfen ad soyad email giriniz");
+                Scanner c =new Scanner(System.in);
+                String genelKisiBilgileri=c.nextLine();
+                dosyaIslemleri.genelUyeEkleme(genelKisiBilgileri);
                 break;
             case 3:
                 mailGonderme.mailGonderme();
                 break;
             default:
                 System.out.println("yanlış sayı tuşladınız");
+                break;
+            }
+
     }
-}}
+}
