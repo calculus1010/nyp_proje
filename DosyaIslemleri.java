be294070ef9f09ec42e98557d0561d671b90d099
calculus1010 <117119873+calculus1010@users.noreply.github.com>
2023-04-09 17:00:00 +0300

    Update DosyaIslemleri.java

--- DosyaIslemleri.java
+++ DosyaIslemleri.java
@@ -1,51 +1,42 @@
-import java.io.*;
-import java.util.Scanner;
-public class Main {
-    private String elitKisiBilgileri;
-    private String genelKisiBilgileri;
-    public String getElitKisiBilgileri(){
-        return this.elitKisiBilgileri;
-    }
-    public void setElitKisiBilgileri(String elitKisiBilgileri){
-        this.elitKisiBilgileri=elitKisiBilgileri;
-    }
-    public String getgenelKisiBilgileri(){
-        return this.genelKisiBilgileri;
-    }
-    public void setgenelKisiBilgileri(String genelKisiBilgileri){
-        this.genelKisiBilgileri=genelKisiBilgileri;
-    }
-    public static void main(String[] args) throws IOException
-    {
-        //diğer clasların nesneleri
-        Main main=new Main();
-        DosyaIslemleri dosyaIslemleri=new DosyaIslemleri();
-        MailGonderme mailGonderme=new MailGonderme();
-        System.out.println("lütfen yapmak istediğiniz işlemi seçiniz \n1-elit üye ekleme\n2-Genel üye ekleme\n3-Mail gönderme");
-        //1.menü
-        Scanner input=new Scanner(System.in);
-        int a= input.nextInt();
-        switch (a)
-            {
-            case 1:
-                System.out.println("lütfen ad soyad email giriniz");
-                Scanner b =new Scanner(System.in);
-                String elitKisiBilgileri=b.nextLine();
-                dosyaIslemleri.elitUyeEkleme(elitKisiBilgileri);
-                break;
-            case 2:
-                System.out.println("lütfen ad soyad email giriniz");
-                Scanner c =new Scanner(System.in);
-                String genelKisiBilgileri=c.nextLine();
-                dosyaIslemleri.genelUyeEkleme(genelKisiBilgileri);
-                break;
-            case 3:
-                mailGonderme.mailGonderme();
-                break;
-            default:
-                System.out.println("yanlış sayı tuşladınız");
-                break;
-            }
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+public class DosyaIslemleri {
+    public static void elitUyeEkleme(String ElitKisiBilgileri) throws IOException
+        {
+            //girilen bilgilerin tanımlanması
+            Main main=new Main();
+            main.setElitKisiBilgileri(ElitKisiBilgileri);
+            String adSoyadEmail = main.getElitKisiBilgileri();
 
+            //elit üyeler için dosya işlemleri
+            File f=new File("kullanıcılar.txt");
+            if(!f.exists())
+            {
+                f.createNewFile();
+            }
+            FileWriter fWriter=new FileWriter(f,true);
+            BufferedWriter bWriter=new BufferedWriter(fWriter);
+            bWriter.write(adSoyadEmail+"\n");
+            bWriter.close();
+
+        }
+    public static void genelUyeEkleme(String GenelKisiBilgileri) throws IOException
+    {
+        //girilen bilgilerin tanımlanması
+        Main main=new Main();
+        main.setgenelKisiBilgileri(GenelKisiBilgileri);
+        String adSoyadEmail=main.getgenelKisiBilgileri();
+        //genel üyeler için dosya işlemleri
+        File p=new File("kullanıcılar.txt");
+        if(!p.exists())
+        {
+            p.createNewFile();
+        }
+        FileWriter fWriter=new FileWriter(p,true);
+        BufferedWriter bWriter=new BufferedWriter(fWriter);
+        bWriter.write(adSoyadEmail+"\n");
+        bWriter.close();
     }
 }
